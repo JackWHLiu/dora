c2c2b8daab26a0dccaa54f28674ee114bfd7f6eb
Alm <924666990@qq.com>
2020-06-15 12:50:37 +0800

    Add EmailPolicy.

---
+++ dora-bugskiller/src/main/java/dora/bugskiller/EmailPolicy.java
@@ -0,0 +1,50 @@
+package dora.bugskiller;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+
+public class EmailPolicy extends CrashReportPolicyWrapper {
+
+    private String mEmail;   //接收者的Email
+    private String mTitle;   //接收者的Email标题
+    private String mEmailServerUrl = "http://47.75.216.2:8080/requestCrashEmail";
+
+    protected EmailPolicy(CrashReportPolicy policy, String email, String title) {
+        super(policy);
+        this.mEmail = email;
+        this.mTitle = title;
+    }
+
+    public void setEmailServerUrl(String url) {
+        this.mEmailServerUrl = url;
+    }
+
+    @Override
+    public void report(CrashInfo info) {
+        super.report(info);
+        OkHttpClient client = new OkHttpClient();
+        HashMap<String,String> params = new HashMap<>();
+        params.put("crash_info", info.toString());
+        params.put("email", mEmail);
+        params.put("title", mTitle);
+        FormBody.Builder builder = new FormBody.Builder();
+        for (String key : params.keySet()) {
+            builder.add(key, params.get(key));
+        }
+        RequestBody body = builder.build();
+        Request request = new Request.Builder()
+                .url(mEmailServerUrl)
+                .post(body)
+                .build();
+        try {
+            client.newCall(request).execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
